dfb0d9bda9e443e9550affa0b0c73565f690d527
Anahi <anahi.z.a@gmail.com>
2019-08-01 12:14:10 -0500

    Lista de objetos polimorfica

--- Program.cs
+++ Program.cs
@@ -15,34 +15,12 @@ namespace CoreEscuela
             Printer.WriteTitle("BIENVENIDOS A LA ESCUELA");
             //Printer.Beep(10000, cantidad: 10);
             ImpimirCursosEscuela(engine.Escuela);
-            Printer.DrawLine(20);
-            Printer.WriteTitle("Pruebas de poliformisfo");
-            var alumnoTest = new Alumno();
-            alumnoTest.Nombre="Claire UnderWood";
-
-            ObjetoEscuelaBase ob = alumnoTest;
-            Printer.WriteTitle("Alumno");
-            WriteLine($"Alumno: {alumnoTest.UniqueId}");
-             WriteLine($"Alumno: {alumnoTest.GetType()}");
-            Printer.WriteTitle("Objeto Escuela");
-            WriteLine($"Objeto: {ob.UniqueId.ToString()}");
-             WriteLine($"Objeto: {ob.GetType()}");
-
-            var eval = new Evaluación(){Nombre="Matematicas", Nota=4.5f};
-       Printer.WriteTitle("Evaluacion");
-            WriteLine($"Evaluacion: {eval.UniqueId.ToString()}");
-             WriteLine($"Evaluacion: {eval.Nota}");
-             WriteLine($"Evaluacion: {eval.GetType()}");
-
-             ob=eval;
-                         Printer.WriteTitle("Objeto Escuela");
-            WriteLine($"Objeto: {ob.UniqueId.ToString()}");
-             WriteLine($"Objeto: {ob.GetType()}");
-
-
-
 
             var listaObjetos = engine.GetObjetosEscuela();
+            foreach (var ob in listaObjetos)
+            {
+                WriteLine(ob.ToString());
+            }
         }
 
         private static void ImpimirCursosEscuela(Escuela escuela)
