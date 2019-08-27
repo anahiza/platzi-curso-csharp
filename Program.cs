209840da476edb1d1d35f931e4b8345d75985b57
Anhy <anahi.z.a@gmail.com>
2019-08-26 19:05:27 -0500

    Manejo de Excepciones

--- Program.cs
+++ Program.cs
@@ -32,8 +32,52 @@ namespace CoreEscuela
             var newEval = new Evaluación();
             string nombre;
             float nota;
+            string notatxt;
             WriteLine("Ingrese el nombre de la evaluación");
             Printer.PressEnter();
+            nombre=Console.ReadLine();
+            if(string.IsNullOrWhiteSpace(nombre))
+            {
+                Printer.WriteTitle("El valor del nombre no puede ser vacio");
+            }
+            else{
+                newEval.Nombre= nombre.ToLower();
+                WriteLine("El nombre ha sido ingresado correctamente");
+            }
+            WriteLine("Ingrese la nota");
+            Printer.PressEnter();
+            notatxt=Console.ReadLine();
+            if(string.IsNullOrWhiteSpace(notatxt))
+            {
+                Printer.WriteTitle("El valor de la nota no puede ser vacio");
+            }
+            else{
+
+                try
+                {
+                    newEval.Nota= float.Parse(notatxt);
+                    if (newEval.Nota <0 || newEval.Nota > 5)
+                    {
+                        throw new ArgumentOutOfRangeException("La nota debe estar entre 0 y 5");
+                    }
+                    else{
+                        WriteLine("La nota ha sido ingresada correctamente");
+
+                    }
+                }
+                catch (ArgumentOutOfRangeException arge)
+                {
+                    WriteLine(arge.Message);
+
+                }
+
+                
+                catch (Exception) {
+                    Printer.WriteTitle("El valor de la nota no es un número válido");
+                }
+
+            }
+
 
 
             
